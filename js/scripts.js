4d3cacb77d9e713345cd20fc724b2b3409e6141f
Anderson Apparecido <andersonsilvapp@gmail.com>
2020-10-12 03:47:33 -0300

    js ok

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+if(window.SimpleSlide) {
+
+    new SimpleSlide({
+        slide: 'quote',
+        time: 5000, 
+        pauseOnHover: true, 
+    });
+
+    new SimpleSlide({
+        slide: 'portfolio',
+        time: 5000, 
+        pauseOnHover: true, 
+        nav: true
+    });
+}
+
+
+if(window.SimpleAnime) {
+    new SimpleAnime();
+}
